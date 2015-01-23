e72c3fe1740644b999fccdea3af7d90c2ae229aa
Jaroslaw Zabiello <hipertracker@gmail.com>
2015-01-23 00:24:29 +0000

    Update package.js

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ var reactAddonsVersion = '0.9.0';
 Package.describe({
     name: 'hipertracker:reactjs',
     summary: 'ReactJS + addons + JSX (--harmony) for Meteor',
-    version: '0.0.5',
+    version: '0.0.6',
     git: 'https://github.com/hipertracker/meteor-reactjs.git'
 });
 
