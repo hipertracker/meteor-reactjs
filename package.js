01ba51d72a16695f06f2c2e81b110498515a7a58
Jaroslaw Zabiello <hipertracker@gmail.com>
2015-04-21 00:07:34 +0100

    update package version, webstorm scr refactoring

--- package.js
+++ package.js
@@ -1,50 +1,50 @@
 var meteorVersion = '1.1.0.2',
-  reactVersion = '0.13.2'
+    reactVersion = '0.13.2';
 
 Package.describe({
-  name: 'rkstar:reactjs',
-  summary: 'ReactJS + addons + JSX (--harmony) for Meteor',
-  version: '0.0.9',
-  git: 'https://github.com/rkstar/meteor-reactjs'
-})
+    name: 'rkstar:reactjs',
+    summary: 'ReactJS + addons + JSX (--harmony) for Meteor',
+    version: '0.0.10',
+    git: 'https://github.com/rkstar/meteor-reactjs'
+});
 
 Package._transitional_registerBuildPlugin({
-  name: 'compileJSX',
-  use: [],
-  sources: [
-    'plugin/compile-jsx.js'
-  ],
-  npmDependencies: {
-    'react': reactVersion,
-    'react-tools': reactVersion
-  }
-})
+    name: 'compileJSX',
+    use: [],
+    sources: [
+        'plugin/compile-jsx.js'
+    ],
+    npmDependencies: {
+        'react': reactVersion,
+        'react-tools': reactVersion
+    }
+});
 
 
 Package.onTest(function (api) {
-  api.use('tinytest')
-  api.use('rkstar:reactjs')
-  api.addFiles('rkstar:reactjs-tests.js')
-})
+    api.use('tinytest');
+    api.use('rkstar:reactjs');
+    api.addFiles('rkstar:reactjs-tests.js');
+});
 
 
 Package.on_use(function (api) {
-  api.versionsFrom(meteorVersion)
+    api.versionsFrom(meteorVersion);
 
-  // Standard distribution of React, same version as react-tools.
-  api.add_files('vendor/react-' + reactVersion + '.js', 'client')
+    // Standard distribution of React, same version as react-tools.
+    api.add_files('vendor/react-' + reactVersion + '.js', 'client');
 
-  // On the server, we use the modules that ship with react.
-  api.add_files('src/require-packages.js', 'server')
-  api.export('React', 'server')
+    // On the server, we use the modules that ship with react.
+    api.add_files('src/require-packages.js', 'server');
+    api.export('React', 'server');
 
-  // Meteor-enabled components should include this mixin via
-  // React.createClass({ mixins: [ReactMeteor.Mixin], ... }).
-  api.add_files('src/ReactMeteor.js', ['server', 'client'])
-  api.export('ReactMeteor', ['server', 'client'])
-})
+    // Meteor-enabled components should include this mixin via
+    // React.createClass({ mixins: [ReactMeteor.Mixin], ... }).
+    api.add_files('src/ReactMeteor.js', ['server', 'client']);
+    api.export('ReactMeteor', ['server', 'client']);
+});
 
 
 Npm.depends({
-  'react': reactVersion
-})
+    'react': reactVersion
+});
--- src/ReactMeteor.js
+++ src/ReactMeteor.js
@@ -1,35 +1,35 @@
 var ReactMeteorMixin = {
     _handleMeteorChange: function () {
-        if( this.getMeteorState !== undefined ){
+        if (this.getMeteorState !== undefined) {
             this.setState(this.getMeteorState())
         }
     },
 
-    _cancelComputation: function(){
-        if( this._meteorComputation ){
+    _cancelComputation: function () {
+        if (this._meteorComputation) {
             this._meteorComputation.stop()
             this._meteorComputation = null
         }
     },
 
-    componentWillMount: function(){
+    componentWillMount: function () {
         this._meteorComputation = Tracker.autorun(this._handleMeteorChange)
     },
 
-    componentWillReceiveProps: function( nextProps ){
+    componentWillReceiveProps: function (nextProps) {
         var oldProps = this.props
         this.props = nextProps
         this._handleMeteorChange()
         this.props = oldProps
     },
 
-    componentWillUnmount: function(){
+    componentWillUnmount: function () {
         this._cancelComputation()
     }
 }
 
 
-if( typeof exports === "object" ){
+if (typeof exports === "object") {
     ReactMeteor = exports
 } else {
     ReactMeteor = {}
